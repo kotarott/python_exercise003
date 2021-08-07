058c8710503bc5008efdfb095ba578e4b7e99362
kotarott <kg221kg@gmail.com>
2021-08-07 10:05:57 +0900

    up

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import eel
 import numpy as np
 
+@eel.expose
+def python_function(val):
+    print(val + " from JS")
+
+@eel.expose
+def python_function2():
+    return "Pythonの戻り値をJSで取得"
+
+
 eel.init("web")
 # jsの結果をpythonで受け取る①　処理後
 eel.js_function()(lambda val: print(val + " from JavaScript"))
