5f338cad3311fb1976abc710c9dc458856a0106f
kotarott <kg221kg@gmail.com>
2021-08-09 17:14:10 +0900

    created exe file

--- search.py
+++ search.py
@@ -1,23 +1,33 @@
 import pandas as pd
+import os
 import eel
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
+def kimetsu_search(word, path):
+    if os.path.exists(path):
+        df = pd.read_csv(path)
+    else:
+        eel.add_results("CSVファイルは存在しません。")
+        return
+
     # 検索対象取得
-    df=pd.read_csv("./source.csv")
-    source=list(df["name"])
+    # df = pd.read_csv("source.csv")
+    source = list(df["name"])
 
     # 検索
     if word in source:
         print("『{}』はあります".format(word))
+        eel.add_results("『{}』はあります".format(word))
     else:
         print("『{}』はありません".format(word))
+        eel.add_results("『{}』はありません".format(word))
+        eel.add_results("『{}』をリストに追加します。".format(word))
         # 追加
-        #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
-        #if add_flg=="1":
+        # add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
+        # if add_flg=="1":
         source.append(word)
     
     # CSV書き込み
-    df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./source.csv",encoding="utf_8-sig")
+    df = pd.DataFrame(source, columns=["name"])
+    df.to_csv(path, encoding="utf_8-sig")
     print(source)
--- view.py
+++ view.py
@@ -2,13 +2,13 @@ import eel
 import desktop
 import search
 
-app_name="html"
+app_name="web"
 end_point="index.html"
 size=(700,600)
 
 @ eel.expose
-def kimetsu_search(word):
-    search.kimetsu_search(word)
+def kimetsu_search(word, path):
+    search.kimetsu_search(word, path)
     
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
